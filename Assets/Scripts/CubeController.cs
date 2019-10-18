a79bc1825f6a9a1437a3aba02fe5020bff2c8d48
Luuiis2415 <jimenez1014luis@gmail.com>
2019-10-18 10:07:17 -0400

    I did Part 1's code

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (GameController.activeCube != null)
+        {
+
+            GameController.activeCube.GetComponent<Renderer>().material.color = Color.white;
+
+        }
+
+        //the one that's been clicked will turn red
+        //and its keeping track of what's been clicked
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        GameController.activeCube = gameObject;
+
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    Vector3 cubePosition;
+
+    //this is one way to do the 2a of part1
+    //after I can try experiementing the other ways
+
+    public static GameObject activeCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+
+            cubePosition = new Vector3(i*2, 0, 0);
+            Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
